4fceaa0286f668820afeeaa31c97464d06df370a
markwylde <markwylde@users.noreply.github.com>
2020-12-23 02:12:59 +0000

    Deploying to gh-pages from  @ 07339bd8bcc3d13c1a786ba2704c16eab5c41149 🚀

--- index.js
+++
@@ -1,8 +0,0 @@
-const render = require('mithril/render');
-const h = require('mithril/hyperscript');
-
-document.addEventListener('DOMContentLoaded', () => {
-  const ui = h('div', 'hello world');
-
-  render(document.body, ui);
-});
